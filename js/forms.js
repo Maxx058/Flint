a02639ece19479111840379a68b6ce7090d064c3
Maxx058 <mohitsk3501@gmail.com>
2022-07-03 14:44:25 +0530

    linking changes

--- js/forms.js
+++ js/forms.js
@@ -1,43 +1,50 @@
-const url = 'http://localhost:5000/enquiry'
+const url = "http://localhost:5000/enquiry";
 
 function submitForm(e) {
-    e.preventDefault();
-    var checked = [];
-    var checkboxes = document.getElementsByName('typeOfProject');
-    for (var i = 0; i < checkboxes.length; i++) {
-        if (checkboxes[i].checked) {
-            checked.push(checkboxes[i].dataset.value);
-        }
+  e.preventDefault();
+  var checked = [];
+  var checkboxes = document.getElementsByName("typeOfProject");
+  for (var i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked) {
+      checked.push(checkboxes[i].dataset.value);
     }
-    var username = document.getElementById("username").value;
-    var email = document.getElementById("email").value;
-    var projectBrief = document.getElementById("projectBrief").value;
-    var projectBudget = document.getElementById("projectBudget").value;
-    postData(url, { username: username, email: email, checked: checked, projectBrief: projectBrief, projectBudget: projectBudget })
-    return false;
+  }
+  var username = document.getElementById("username").value;
+  var email = document.getElementById("email").value;
+  var projectBrief = document.getElementById("projectBrief").value;
+  var projectBudget = document.getElementById("projectBudget").value;
+  postData(url, {
+    username: username,
+    email: email,
+    checked: checked,
+    projectBrief: projectBrief,
+    projectBudget: projectBudget,
+  });
+  return false;
 }
 
-const postData = async(url = '', data = {}) => {
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            name: data.username,
-            email: data.email,
-            details: data.projectBrief,
-            budget: data.projectBudget,
-            requirements: data.checked
-        })
-    });
-    try {
-        console.log(await response.json());
-    } catch (error) {
-        console.log("error", error);
-    }
-}
+const postData = async (url = "", data = {}) => {
+  window.location = "/index.html";
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: data.username,
+      email: data.email,
+      details: data.projectBrief,
+      budget: data.projectBudget,
+      requirements: data.checked,
+    }),
+  });
 
+  try {
+    console.log(await response.json());
+  } catch (error) {
+    console.log("error", error);
+  }
+};
 
 var form = document.getElementById("queryForm");
 form.addEventListener("submit", submitForm);
